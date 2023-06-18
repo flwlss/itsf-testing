1c2b67aac2d60cb8c55f958aaa76fd061bab24ac
Dmitry <dimaandreev1337@gmail.com>
2023-06-18 19:22:09 +0700

    show card info

--- src/components/ContentItem.tsx
+++ src/components/ContentItem.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../assets/styles/Content.module.css'
+import CustomButton from "./CustomButton";
 
 interface IContentItemProps {
   email: string;
@@ -9,13 +10,26 @@ interface IContentItemProps {
 const ContentItem = (props: IContentItemProps) => {
 
   const randomRating = Math.floor(Math.random() * (Math.max(6) - Math.min(1)) + Math.min(1))
+  const [showCard, setShowCard] = useState(false)
 
   return (
     <article
+      onMouseEnter={() => { setShowCard(true) }}
+      onMouseLeave={() => { setShowCard(false) }}
       className={styles.item}>
       <img className={styles.itemImg} src="img/waterfall.jpg" alt="" />
-      <div className={styles.itemInfo}>
-        {randomRating}
+      <div className={showCard ? styles.itemInfo : `${styles.itemInfo} ${styles.itemInfoActive}`}>
+        <div className={styles.itemInfoWrapper}>
+          <div className={styles.itemInfoEmailRating}>
+            <p>{props.email}</p>
+            <div className={styles.itemInfoRating}>
+              <p>{randomRating}</p>
+              <img src="/img/star.svg" alt="" />
+            </div>
+          </div>
+          <p className={styles.itemInfoText}>{props.body}</p>
+        </div>
+        <CustomButton onClick={() => { }} title="Visit comment" />
       </div>
     </article>
   )
